2507543e9875df9317d221340e98ba234e0e8a55
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2022-03-01 17:45:49 -0500

    Made the all products component display dummy data :)

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,16 +2,58 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+// designName:
+// price:
+// img:
+
+const dummyData = [
+  {
+    id: 1,
+    designName: "A Cool T-shirt",
+    price: 20,
+    img: "https://smwv.org/wp-content/uploads/2021/01/tshirt-2.jpg",
+  },
+  {
+    id: 2,
+    designName: "A Pretty Cool T-shirt",
+    price: 80,
+    img: "https://smwv.org/wp-content/uploads/2021/01/tshirt-2.jpg",
+  },
+  {
+    id: 3,
+    designName: "A Not Very Cool T-shirt",
+    price: 60,
+    img: "https://smwv.org/wp-content/uploads/2021/01/tshirt-2.jpg",
+  },
+  {
+    id: 4,
+    designName: "A Kind Of Cool T-shirt",
+    price: 210,
+    img: "https://smwv.org/wp-content/uploads/2021/01/tshirt-2.jpg",
+  },
+  {
+    id: 5,
+    designName: "A Super Cool T-shirt",
+    price: 220,
+    img: "https://smwv.org/wp-content/uploads/2021/01/tshirt-2.jpg",
+  },
+];
+
 export const AllProducts = () => {
-    return (
-        <div>
-        <h1>Welcome to The All Products Page</h1>
-        <ul>
-          <li>one</li>
-          <li>two</li>
-          <li>three</li>
-          <li>four</li>
-        </ul>
+  return (
+    <div>
+      <h1>Welcome to The All Products Page</h1>
+      <div>
+        {dummyData.map((tShirt) => {
+          return (
+            <div key={tShirt.id}>
+              <img src={tShirt.img} />
+              <div>{tShirt.designName}</div>
+              <div>${tShirt.price}</div>
+            </div>
+          );
+        })}
       </div>
-    )
+    </div>
+  );
 };
