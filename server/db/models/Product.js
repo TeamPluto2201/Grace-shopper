ac1dd07435e415d11b0523fdbdd2528bc3fc328c
Mica Oz <kahulaoz@gmail.coml>
2022-03-01 15:09:28 -0700

    product model

---
+++ server/db/models/Product.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Product = db.define("user", {
+  garmentType: {
+    type: Sequelize.STRING,
+    defaulValue: T - Shirt,
+    allowNull: false,
+  },
+  designName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  designName: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  imgPath: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
