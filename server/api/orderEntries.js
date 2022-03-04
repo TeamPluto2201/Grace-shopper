cd13dd751fec25c0da87807c512de3a7cfd998be
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2022-03-03 19:59:28 -0500

    rewrote our OrderEntry route to query the database for a users current order Id and to create a new Order Entry with the associated Id's. Also, tweaked the seed file to better reflect the logic of our order system

--- server/api/orderEntries.js
+++ server/api/orderEntries.js
@@ -1,17 +1,34 @@
 const router = require("express").Router();
 const {
-    models: { OrderEntry },
+    models: { OrderEntry, Order },
 } = require("../db");
 
 router.post("/", async (req, res, next) => {
     try {
-        console.log("REQ.BODY**********************", req.body)
-        const orderEntry = await OrderEntry.create(req.body);
+        // grab the current user Id and find their associated order that has not been purchased
+        const currentUserId = req.body.userId
+        const currentOrder = await Order.findAll({
+            where: {
+                userId: currentUserId,
+                purchased: false
+            }
+        })
+        // create a new object that includes the req.body
+        // key/value pairs, subtracts the userId key, and adds the ID from the current order
+        const newlyPlacedOrder = {
+            size: req.body.size,
+            color: req.body.color,
+            QTY: req.body.QTY,
+            productId: req.body.productId,
+            orderId: currentOrder[0].dataValues.id
+        }
+        //use that object to create a new order entry
+        const orderEntry = await OrderEntry.create(newlyPlacedOrder);
+        console.log(orderEntry)
         res.send(orderEntry);
     } catch (err) {
         next(err);
     }
 });
-
 
 module.exports = router;
