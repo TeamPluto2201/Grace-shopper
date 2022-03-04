4adcf2e561954458618768d4e0457b4d304bd40c
Diana Viglucci <dvigl86@gmail.com>
2022-03-03 20:27:20 -0500

    admins can view Admin Dashboard but regular users can not

--- client/Routes.js
+++ client/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { withRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
 import Welcome from "./components/Welcome";
 import { me } from "./store";
@@ -17,33 +17,41 @@ class Routes extends Component {
   }
 
   render() {
-
     const isLoggedIn = this.props.isLoggedIn;
     const isAdmin = this.props.isAdmin;
 
     return (
       <div>
-        {isLoggedIn ? (
-          <Switch>
-            {/* <Route path="/home" component={Home} /> */}
-            <Welcome />
-            {/* <Route path='/AllProducts' component={AllProducts} /> */}
-            {/* <Redirect to="/home" /> */}
-          </Switch>
+        {isAdmin && isLoggedIn ? (
+          <div>
+            {/* <AdminDash /> */}
+            <Link to={"/admin"}>View Dashboard</Link>
+          </div>
         ) : (
-          <Switch>
-            {/* <Route path='/' exact component={ Login } /> */}
-            {/* <Login />
-            <Signup /> */}
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-          </Switch>
+          <div>
+            {isLoggedIn ? (
+              <div>
+                {/* <Route path="/home" component={Home} /> */}
+                <Welcome />
+                {/* <Route path='/AllProducts' component={AllProducts} /> */}
+                {/* <Redirect to="/home" /> */}
+              </div>
+            ) : (
+              <div>
+                {/* <Route path='/' exact component={ Login } /> */}
+                {/* <Login />
+              <Signup /> */}
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+              </div>
+            )}
+          </div>
         )}
 
-        <Route exact path='/' component={AllProducts} />
-        <Route exact path='/products' component={AllProducts} />
-        <Route exact path='/products/:id' component={SingleProduct} />
-        <Route exact path='/admin' component={AdminDash} />
+        <Route exact path="/" component={AllProducts} />
+        <Route exact path="/products" component={AllProducts} />
+        <Route exact path="/products/:id" component={SingleProduct} />
+        <Route exact path="/admin" component={AdminDash} />
       </div>
     );
   }
@@ -57,7 +65,7 @@ const mapState = (state) => {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     isLoggedIn: !!state.auth.id,
-    isAdmin: !!state.auth.isAdmin
+    isAdmin: !!state.auth.isAdmin,
   };
 };
 
--- client/components/AdminDash.js
+++ client/components/AdminDash.js
@@ -1,76 +1,79 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Switch, Link } from "react-router-dom";
 import { getProductsThunkCreator } from "../store/allProducts";
 import { getUsersThunkCreator } from "../store/adminDash";
-
 
 class AdminDash extends React.Component {
   constructor() {
     super();
-  };
+  }
 
   componentDidMount() {
-      this.props.loadAllProducts();
-      this.props.loadAllUsers();
-  };
+    this.props.loadAllProducts();
+    this.props.loadAllUsers();
+  }
 
   render() {
     const productsToRender = this.props.products || [];
     const usersToRender = this.props.users || [];
+    const isAdmin = this.props.isAdmin;
 
     return (
-        <div>
-            <h1>Admin Dashboard</h1>
-            <h3>
-                Products
-            </h3>
+      <div>
+        {isAdmin ? (
             <div>
-                {productsToRender.map(element => {
-                    return (
-                        <div key={element.id}>
-                            <div>{element.designName}</div>
-                            <img src={element.imgPath} style={{width: '100px'}}/>
-                            <ul>
-                                <li>ID: {element.id}</li>
-                                <li>Price: {element.price}</li>
-                            </ul>
-                        </div>
-                    )
+              <h1>Admin Dashboard</h1>
+              <h3>Products</h3>
+              <div>
+                {productsToRender.map((element) => {
+                  return (
+                    <div key={element.id}>
+                      <div>{element.designName}</div>
+                      <img src={element.imgPath} style={{ width: "100px" }} />
+                      <ul>
+                        <li>ID: {element.id}</li>
+                        <li>Price: {element.price}</li>
+                      </ul>
+                    </div>
+                  );
                 })}
+              </div>
+              <h3>Users</h3>
+              <ul>
+                {usersToRender.map((element) => {
+                  return <li key={element.id}>{element.username}</li>;
+                })}
+              </ul>
             </div>
-            <h3>
-                Users
-            </h3>
-            <ul>
-                {usersToRender.map(element => {
-                    return (
-                        <li key={element.id}>
-                            {element.username}
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
-  };
-};
+        ) : (
+            <div>
+                <div>You're not allowed here, son.</div>
+                <Link to={"/products"}>Go home.</Link>
+            </div>
+        )}
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
-    return {
-        products: state.products,
-        users: state.users,
-    };
-};
+  return {
+    products: state.products,
+    users: state.users,
+    isAdmin: !!state.auth.isAdmin,
+  };
+}
 
 function mapDispatchToProps(dispatch) {
-    return {
-        loadAllProducts: () => {
-            dispatch(getProductsThunkCreator());
-        },
-        loadAllUsers: () => {
-            dispatch(getUsersThunkCreator());
-        }
-    };
-};
+  return {
+    loadAllProducts: () => {
+      dispatch(getProductsThunkCreator());
+    },
+    loadAllUsers: () => {
+      dispatch(getUsersThunkCreator());
+    },
+  };
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminDash);
