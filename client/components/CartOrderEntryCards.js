d97ee474ff89e2425232707717351554cff9d14b
choi2010 <lindachoi05@gmail.com>
2022-03-08 11:04:54 -0500

    updated cart

--- client/components/CartOrderEntryCards.js
+++ client/components/CartOrderEntryCards.js
@@ -1,19 +1,49 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { getOrderEntryThunkCreator } from "../store/cart";
+import { getOrderEntryThunkCreator, deleteOrderEntryThunkCreator, updateOrderEntryThunkCreator } from "../store/cart";
 
-export default class CartItem extends React.Component {
+// export default 
+class CartItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       size: "",
       color: "",
       QTY: "",
-      designName: "",
-      imgPath: "",
-      price: "",
+      id: ""
+      // designName: "",
+      // imgPath: "",
+      // price: "",
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClickUpdateButton = this.handleClickUpdateButton.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleClick(event) {
+    console.log("THIS IS THE CLICK ON THE BUTTON*******", event.target)
+    this.props.deleteOrderEntry(event.target.value)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // console.log("this is event on the form ", event.target.value)
+
+    // const changes = {...this.state, event.}
+    console.log("This is the state that will be passed into update", this.state)
+    this.props.updateOrderEntry(this.state);
+  }
+  handleClickUpdateButton(event) {
+    this.setState({ id: event.target.value })
   }
 
   async componentDidMount() {
@@ -21,10 +51,38 @@ export default class CartItem extends React.Component {
     console.log("HERE!!!!!!!!orderEntry mounted");
   }
 
+  // render() {
+  //   console.log("PROPS INSIDE CART ORDER ENTRY CARD--->", this.props);
+  //   console.log("this.props.entryArray", this.props.entryArray)
+
+  //   return this.props.entryArray.map((entry) => {
+
+  //     return (
+  //       <div id='productCardAllView' key={entry.id}>
+  //         <img id='shirtImgAll' src={entry.product.imgPath} />
+  //         <div id='designNameAll'>{entry.product.designName}</div>
+  //         <div>${(entry.product.price / 100).toFixed(2)}</div>
+  //         <div>number of items {entry.QTY}</div>
+  //         <div>size {entry.size}</div>
+  //         {/* <div>color {entry.colorId}</div> */}
+  //         <div>color {entry.color.name}</div>
+  //         <button value={entry.id} type='button'>Update Item</button>
+  //         <button onClick={this.handleClick} value={entry.id} type='button'>Remove Item</button>
+  //       </div>
+  //     );
+  //   });
+  // }
+
+
+  //////////////////////////////////////
+
+
   render() {
     console.log("PROPS INSIDE CART ORDER ENTRY CARD--->", this.props);
+    console.log("this.props.entryArray", this.props.entryArray)
 
     return this.props.entryArray.map((entry) => {
+
       return (
         <div id='productCardAllView' key={entry.id}>
           <img id='shirtImgAll' src={entry.product.imgPath} />
@@ -32,12 +90,51 @@ export default class CartItem extends React.Component {
           <div>${(entry.product.price / 100).toFixed(2)}</div>
           <div>number of items {entry.QTY}</div>
           <div>size {entry.size}</div>
-          <div>color {entry.colorId}</div>
-          <button type='button'>x</button>
+          {/* <div>color {entry.colorId}</div> */}
+          <div>color {entry.color.name}</div>
+
+          <div>Update Item</div>
+          <form value={entry.id} onSubmit={this.handleSubmit}>
+
+            <div>
+              <label>Size</label>
+              <select name='size' onChange={this.handleChange}>
+
+                <option value={"S"}>S</option>
+                <option value={"M"}>M</option>
+                <option value={"L"}>L</option>
+
+              </select>
+            </div>
+
+            <div>
+              <label>Color</label>
+              <select name='color.name' onChange={this.handleChange}>
+                <option value={"white"}>white</option>
+                <option value={"black"}>black</option>
+              </select>
+            </div>
+
+            <div>
+              <label>Qty</label>
+
+              <input type='number' min='1' name='QTY' value={this.state.name} onChange={this.handleChange} />
+
+            </div>
+            <div>
+              <button value={entry.id} type='submit' onClick={this.handleClickUpdateButton}>Update Item </button>
+            </div>
+
+          </form>
+
+          <button onClick={this.handleClick} value={entry.id} type='button'>Remove Item</button>
         </div>
       );
     });
   }
+
+
+
 }
 
 // function mapStateToProps(state) {
@@ -55,4 +152,17 @@ export default class CartItem extends React.Component {
 //   };
 // }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CartItem);
+function mapDispatchToProps(dispatch) {
+  return {
+    deleteOrderEntry: (id) => {
+      dispatch(deleteOrderEntryThunkCreator(id));
+    },
+
+    updateOrderEntry: (item) => {
+      dispatch(updateOrderEntryThunkCreator(item))
+    }
+  };
+}
+
+
+export default connect(null, mapDispatchToProps)(CartItem);
--- server/api/users.js
+++ server/api/users.js
@@ -1,29 +1,28 @@
 const router = require('express').Router()
-const { models: { User }} = require('../db')
-const { requireAuth } = require('../Middleware/authMiddleware')
+const { models: { User } } = require('../db')
 
 router.get('/', async (req, res, next) => {
   try {
     //if we have a token
-      if (req.headers.authorization) {
-        //use that token to find information about the user from the database
-        const currentUser = await User.findByToken(req.headers.authorization)
-        //if that user is an Administrator
-        if (currentUser.isAdmin) {
+    if (req.headers.authorization) {
+      //use that token to find information about the user from the database
+      const currentUser = await User.findByToken(req.headers.authorization)
+      //if that user is an Administrator
+      if (currentUser.isAdmin) {
         //give them access to information about all users
         const users = await User.findAll({
-            // explicitly select only the id and username fields - even though
-            // users' passwords are encrypted, it won't help if we just
-            // send everything to anyone who asks!
-            attributes: ['id', 'username', 'isAdmin']
-          })
-          res.json(users)
-        } else {
-          res.redirect('/')
-        }
+          // explicitly select only the id and username fields - even though
+          // users' passwords are encrypted, it won't help if we just
+          // send everything to anyone who asks!
+          attributes: ['id', 'username', 'isAdmin']
+        })
+        res.json(users)
       } else {
         res.redirect('/')
       }
+    } else {
+      res.redirect('/')
+    }
   } catch (err) {
     next(err)
     console.log(err)
@@ -32,14 +31,14 @@ router.get('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
   try {
-    const [ userToUpdate ] = await User.findAll({
+    const [userToUpdate] = await User.findAll({
       where: {
         id: req.params.id
       },
       attributes: ['id', 'username', 'isAdmin']
     })
     res.send(await userToUpdate.update({ isAdmin: true }));
-  } catch(err) {
+  } catch (err) {
     next(err);
   };
 });
